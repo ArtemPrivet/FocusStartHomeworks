f63b24257af0c17af97724f546c1e87902b65272
Artem Orlov <ar.orlov@cft.ru>
2019-10-26 20:59:12 +0300

    Add Test project

---
+++ Test/Test/Animal.swift
@@ -0,0 +1,23 @@
+//
+//  Animal.swift
+//  Test
+//
+//  Created by Artem Orlov on 26/10/2019.
+//
+
+struct Animal {
+	let name :String
+	let legCount: Int
+
+	func move() {
+		print("\(name) with \(legCount) legs is moving")
+	}
+
+	init?(name: String, legCount: Int) {
+		guard legCount >= 0 else {
+			return nil
+		}
+		self.name = name
+		self.legCount = legCount
+	}
+}
