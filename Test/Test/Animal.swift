a8f112ed028e231fafa27831f52283cead3405a1
Ed <sinyakov.gsg@gmail.com>
2019-10-30 20:42:41 +0300

    firstCommit

--- Test/Test/Animal.swift
+++ Test/Test/Animal.swift
@@ -5,9 +5,11 @@
 //  Created by Artem Orlov on 26/10/2019.
 //
 
-struct Animal {
-	let name :String
+struct Animal
+{
+	let name: String
 	let legCount: Int
+
 	func move() {
 		print("\(name) with \(legCount) legs is moving")
 	}
@@ -19,7 +21,4 @@ struct Animal {
 		self.name = name
 		self.legCount = legCount
 	}
-
-
-	
 }
