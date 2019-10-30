7dfa8ef72ed6c02996521f5e072d95ed9b11d957
Саша Руцман <rutsman@bk.ru>
2019-10-30 20:53:27 +0300

    Изменил Animal и AppDelegate

--- Test/Test/Animal.swift
+++ Test/Test/Animal.swift
@@ -5,13 +5,15 @@
 //  Created by Artem Orlov on 26/10/2019.
 //
 
-struct Animal {
-	let name :String
+struct Animal
+{
+	let name: String
 	let legCount: Int
+	
 	func move() {
 		print("\(name) with \(legCount) legs is moving")
 	}
-
+	
 	init?(name: String, legCount: Int) {
 		guard legCount >= 0 else {
 			return nil
@@ -19,7 +21,4 @@ struct Animal {
 		self.name = name
 		self.legCount = legCount
 	}
-
-
-	
 }
--- Test/Test/AppDelegate.swift
+++ Test/Test/AppDelegate.swift
@@ -10,23 +10,22 @@ import UIKit
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate
 {
-
+	
 	var window: UIWindow?
-
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-		let Dog = Animal.init(name: "Rex", legCount: 4)
-		let cat = Animal(name: "Barcik", legCount: 3)!
-
-		let spider = Animal(name: "Piter", legCount: -8)
-
-
-		let animals = [Dog, cat, spider]
-		for animal in animals {
-			animal!.move()
-		}
-
-		return true
+	
+	func application(_ application: UIApplication,
+					 didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?)
+		-> Bool {
+			
+			let dog = Animal(name: "Rex", legCount: 4)
+			let cat = Animal(name: "Barcik", legCount: 3)
+			let spider = Animal(name: "Piter", legCount: 8)
+			
+			let animals = [dog, cat, spider]
+			for animal in animals {
+				animal?.move()
+			}
+			
+			return true
 	}
 }
-
