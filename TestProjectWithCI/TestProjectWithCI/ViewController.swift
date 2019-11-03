c312a42bbf5d797a39d104d8b271e1d19f83a4a1
Roman <misnikovroman@gmail.com>
2019-11-03 12:55:42 +0300

    Настроил пустой проект

---
+++ TestProjectWithCI/TestProjectWithCI/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TestProjectWithCI
+//
+//  Created by MisnikovRoman on 03.11.2019.
+//  Copyright © 2019 MisnikovRoman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
