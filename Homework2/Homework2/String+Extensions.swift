5ebbee9e555963afca10dc144805f91da61f478f
Maxim Shalashnikov <schalaschnikov@yandex.ru>
2019-11-08 13:29:45 +0300

    Решение двух заданий

--- Homework2/Homework2/String+Extensions.swift
+++ Homework2/Homework2/String+Extensions.swift
@@ -22,10 +22,60 @@
 extension String
 {
 	func reversedWords() -> String {
-		return ""
+		let splitedString = split(separator: " ")
+		var reversedWords = ""
+		for word in splitedString{
+			if word == splitedString.last {
+				reversedWords += word.reversed()
+			}
+			else {
+				reversedWords += word.reversed() + " "
+			}
+		}
+		return reversedWords
 	}
 
 	func validate() -> Bool {
+		//Проверяем наличие символов в строке
+		for char in self {
+			if (char >= "a" && char <= "z") || (char >= "A" && char <= "Z") {
+			   return false
+			}
+		}
+		//Удаляем все лишнее
+		let filteredStr = self.filter("+0123456789.".contains)
+		//Если номер начинается с "+7"
+		if filteredStr.count == 12 {
+			let firstIndex = filteredStr.startIndex
+			let secondIndex = filteredStr.index(firstIndex, offsetBy: 1)
+			if filteredStr[firstIndex] == "+" && filteredStr[secondIndex] == "7" {
+				if codeValidation(filteredStr: filteredStr, start: secondIndex) {
+					return true
+				}
+			}
+		}
+		//Если номер начинается с  7 или 8
+		if filteredStr.count == 11 {
+			let firstIndex = filteredStr.startIndex
+			if filteredStr[firstIndex] == "7" || filteredStr[firstIndex] == "8" {
+				if codeValidation(filteredStr: filteredStr, start: firstIndex) {
+					return true
+				}
+			}
+		}
+		return false
+	}
+
+	private func codeValidation(filteredStr: String, start: String.Index) -> Bool {
+
+		let firstIndex = filteredStr.index(start, offsetBy: 1)
+		let secondIndex = filteredStr.index(firstIndex, offsetBy: 3)
+		let buffer = Int(filteredStr[firstIndex..<secondIndex])
+		if let code = buffer {
+			if code >= 900 && code <= 999 {
+				return true
+			}
+		}
 		return false
 	}
 }
