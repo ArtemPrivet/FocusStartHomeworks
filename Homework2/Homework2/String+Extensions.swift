9e4fb8b92c6c8329d3e042d06a978fda0280f57d
Stanislav Kozlov <bleymy@mail.ru>
2019-11-08 11:45:56 +0300

    Реализация функций по домашнему заданию 2

--- Homework2/Homework2/String+Extensions.swift
+++ Homework2/Homework2/String+Extensions.swift
@@ -18,14 +18,29 @@
 // с '+7', '7' и '8'. Номер можно вводить и со скобками и с черточками
 // и пробелами.
 // Сигнатура метода: .validate() -> Bool
+import Foundation
 
 extension String
 {
 	func reversedWords() -> String {
-		return ""
+		let array = self.components(separatedBy: " ")
+		let result = String(array.map { String($0.reversed()) }.reduce("", { $0 + $1 + " " }).dropLast())
+		return result
 	}
 
 	func validate() -> Bool {
-		return false
+		let phoneNumber = self.replacingOccurrences(of: " ", with: "")
+		let pattern = "^(8|7|\\+7)[\\(]?(9)\\d{2}[\\)]?\\d{3}[\\-]?\\d{2}[\\-]?\\d{2}$"
+		do {
+			let regex = try NSRegularExpression(pattern: pattern,
+												options: .caseInsensitive)
+			let result = regex.firstMatch(in: phoneNumber,
+										  options: NSRegularExpression.MatchingOptions(rawValue: 0),
+										  range: NSRange(location: 0, length: phoneNumber.count)) != nil
+			return result
+		}
+		catch {
+			return false
+		}
 	}
 }
