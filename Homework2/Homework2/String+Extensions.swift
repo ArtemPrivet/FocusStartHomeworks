eeceaa71f9a45598cc790f35396fb47dc79b7383
Кирилл Федоров <ikirillfedorov@gmail.com>
2019-11-09 11:55:47 +0300

    Добавлено расширения для строк

--- Homework2/Homework2/String+Extensions.swift
+++ Homework2/Homework2/String+Extensions.swift
@@ -18,14 +18,22 @@
 // с '+7', '7' и '8'. Номер можно вводить и со скобками и с черточками
 // и пробелами.
 // Сигнатура метода: .validate() -> Bool
+import Foundation
 
 extension String
 {
 	func reversedWords() -> String {
-		return ""
+		let wordArray = self.components(separatedBy: " ")
+		let reversedWordsArray = wordArray.map { String($0.reversed()) }
+		return reversedWordsArray.joined(separator: " ")
 	}
 
 	func validate() -> Bool {
-		return false
+		let regex = "^(\\+7|7|8)+" +
+					"[ \\-]?\\(?[9][0-9]{2}\\)?" +
+					"[ \\-]?[0-9]{3}" +
+					"[ \\-]?[0-9]{2}" +
+					"[ \\-]?[0-9]{2}$"
+		return NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: self)
 	}
 }
