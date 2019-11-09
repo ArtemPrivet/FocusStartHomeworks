bf48adcbc2fdb9110ddeb9d887e8986f49c7373b
Саша Руцман <rutsman@bk.ru>
2019-11-09 11:54:43 +0300

    Исправил методы: reverseWords, validate

--- Homework2/Homework2/String+Extensions.swift
+++ Homework2/Homework2/String+Extensions.swift
@@ -18,14 +18,40 @@
 // с '+7', '7' и '8'. Номер можно вводить и со скобками и с черточками
 // и пробелами.
 // Сигнатура метода: .validate() -> Bool
+import Foundation
 
 extension String
 {
 	func reversedWords() -> String {
 		return ""
+		var reversedWordsArr = [String]()
+		let wordsArr = self.split(separator: " ")
+		_ = wordsArr.map {
+			var lettersArr = [Character]()
+			var reversedWord = ""
+			for letter in $0 {
+				lettersArr.append(letter)
+			}
+			lettersArr.reverse()
+			_ = lettersArr.map {
+				reversedWord += String($0)
+			}
+			reversedWordsArr.append(reversedWord)
+		}
+		return reversedWordsArr.joined(separator: " ")
 	}
-
+	
 	func validate() -> Bool {
 		return false
+		let regex = "^((8|\\+7|7)[\\-]?)(\\(?9\\d{2}\\)?[\\-]?)(\\d{3}[\\-]?)(\\d{2}[\\-]?)(\\d{2}[\\-]?)$"
+		let phoneTest = NSPredicate(format: "SELF MATCHES %@", regex)
+		return phoneTest.evaluate(with: self.split(separator: " ").joined(separator: ""))
+		// ((8|\\+7|7)[\\- ]?) 8 или +7 или 7, "-" опциональный элемент
+		// (\\(?9 "(" опциональный элемент, 9 обязательный
+		// \\d{2} сопоставление, чтобы было 2 цифры
+		// \\)? ")" - опциональный элемент
+		// [\\- ]?) - "-" опциональный элемент
+		// (\\d{3}[\\- ]?) - сопоставление, чтобы было 3 цифры, после которых "-" опциональный элемент
 	}
+	
 }
