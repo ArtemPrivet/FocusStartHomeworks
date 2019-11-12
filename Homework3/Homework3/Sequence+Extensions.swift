76ba15f1c72d8d4ce867307764c68d70549a29fb
MisnikovRoman <misnikovroman@gmail.com>
2019-11-12 13:17:18 +0300

    Добавил шаблон для заполнения

---
+++ Homework3/Homework3/Sequence+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Calculator.swift
+//  Homework2
+//
+//  Created by MisnikovRoman on 03.11.2019.
+//  Copyright © 2019 MisnikovRoman. All rights reserved.
+//
+
+extension Sequence
+{
+	func customMap() {
+		// ...
+	}
+
+	func customReduce() {
+		// ...
+	}
+
+	func customCompactMap() {
+		// ...
+	}
+}
