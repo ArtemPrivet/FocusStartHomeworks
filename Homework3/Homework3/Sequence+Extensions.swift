efbef32bf4348bd2b533b569bedcfe3e20101ed7
Mikhail Medvedev <icodedbear@gmail.com>
2019-11-12 17:09:30 +0300

    Добавил реализацию customMap(), customReduce(), customCompactMap()

--- Homework3/Homework3/Sequence+Extensions.swift
+++ Homework3/Homework3/Sequence+Extensions.swift
@@ -8,15 +8,29 @@
 
 extension Sequence
 {
-	func customMap() {
-		// ...
+	func customMap<T>(transform: (Element) -> T) -> [T] {
+		var transformed = [T]()
+		forEach { element in
+			transformed.append(transform(element))
+		}
+		return transformed
 	}
 
-	func customReduce() {
-		// ...
+	func customReduce<Result>(_ ininital: Result, updateAccumulatingResult: (inout Result, Element) -> Result) -> Result {
+		var result = ininital
+		forEach { element in
+			result = updateAccumulatingResult(&result, element)
+		}
+		return result
 	}
 
-	func customCompactMap() {
-		//...
+	func customCompactMap<T>(transform: (Element) -> T?) -> [T] {
+		var transformed = [T]()
+		forEach { element in
+			if let transformedElement = transform(element) {
+				transformed.append(transformedElement)
+			}
+		}
+		return transformed
 	}
 }
