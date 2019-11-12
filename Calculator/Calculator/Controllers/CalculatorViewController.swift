2e0a23c6dd27b9bc089fe0f90d83cc926af256c8
Artem Orlov <ar.orlov@cft.ru>
2019-11-12 14:28:22 +0300

    Добавил базовый проект

---
+++ Calculator/Calculator/Controllers/CalculatorViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorViewController.swift
+//  Calculator
+//
+//  Created by Artem Orlov on 12/11/2019.
+//  Copyright © 2019 Artem Orlov. All rights reserved.
+//
+
+import UIKit
+
+final class CalculatorViewController: UIViewController
+{
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
