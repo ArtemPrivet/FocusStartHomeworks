57ce15e3246d06d0c9d32bf4ac02093ca3e469b9
Maxim Shalashnikov <schalaschnikov@yandex.ru>
2019-11-12 21:09:44 +0300

    Revert "Вернул let свойствам в тестах"
    This reverts commit 62f8bd178ce85d9bfea682be4b91fc88e6a634da.

--- Homework3/Homework3/Sequence+Extensions.swift
+++ Homework3/Homework3/Sequence+Extensions.swift
@@ -10,24 +10,24 @@ extension Sequence
 {
 	func customMap<T>(_ transform: (Element) -> (T)) -> [T] {
 		 var mapped = [T]()
-		 for element in self {
-			 mapped.append(transform(element))
+		 for elem in self {
+			 mapped.append(transform(elem))
 		 }
 		 return mapped
 	 }
 
 	 func customReduce<T>(_ initialResult: T, _ nextPartialResult: (T, Element) -> (T)) -> T {
 		 var initialResult = initialResult
-		 for element in self {
-			 initialResult = nextPartialResult(initialResult, element)
+		 for elem in self {
+			 initialResult = nextPartialResult(initialResult, elem)
 		 }
 		 return initialResult
 	 }
 
 	 func customCompactMap<T>(_ transform: (Element) -> (T?)) -> [T] {
 		 var mapped = [T]()
-		 for element in self {
-			if let transformed = transform(element) {
+		 for elem in self {
+			if let transformed = transform(elem) {
 				mapped.append(transformed)
 			}
 		}
