b6572df2d77fcae13a624e40b628bb5d1c09f755
Mikhail Medvedev <icodedbear@gmail.com>
2019-11-16 18:24:23 +0300

    Создал window, и назначил rootViewController.

--- Calculator/Calculator/Application/AppDelegate.swift
+++ Calculator/Calculator/Application/AppDelegate.swift
@@ -11,9 +11,16 @@ import UIKit
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate
 {
+	var window: UIWindow?
+
 	func application(_ application: UIApplication,
 					 didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
+
+		let calculatorViewController = CalculatorViewController()
+		window = UIWindow(frame: UIScreen.main.bounds)
+		window?.makeKeyAndVisible()
+		window?.rootViewController = calculatorViewController
+
 		return true
 	}
 }
