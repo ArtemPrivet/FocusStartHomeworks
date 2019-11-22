fb8e77ff934fb21df4a057ca791f6a9ed79780cc
Arkadiy <dragon_500@mail.ru>
2019-11-22 21:10:42 +0300

    create Stack and Queue

---
+++ Calculator/Calculator/Models/Queue.swift
@@ -0,0 +1,31 @@
+////
+////  Queue.swift
+////  Calculator
+////
+////  Created by Arkadiy Grigoryanc on 22.11.2019.
+////  Copyright © 2019 Artem Orlov. All rights reserved.
+////
+//
+//protocol IQueue
+//{
+//	associatedtype Element
+//	mutating func push(_ element: Element)
+//	mutating func pop() -> Element?
+//}
+//
+//struct Queue<Element>: IQueue
+//{
+//	private var queue = [Element]()
+//
+//	mutating func push(_ element: Element) { queue.append(element) }
+//
+//	mutating func pop() -> Element? {
+//		guard queue.isEmpty == false else { return nil }
+//		return queue.removeFirst()
+//	}
+//}
+//
+//extension Queue: CustomStringConvertible
+//{
+//	var description: String { "\(queue)" }
+//}
---
+++ Calculator/Calculator/Models/Stack.swift
@@ -0,0 +1,36 @@
+//
+//  Stack.swift
+//  Calculator
+//
+//  Created by Arkadiy Grigoryanc on 19.11.2019.
+//  Copyright © 2019 Artem Orlov. All rights reserved.
+//
+
+protocol IStack
+{
+	associatedtype Element
+	mutating func removeAll()
+	mutating func push(_ element: Element)
+	mutating func pop() -> Element?
+}
+
+struct Stack<Element>: IStack
+{
+
+	var isEmpty: Bool { array.isEmpty }
+	var count: Int { array.count }
+	var top: Element? { array.last }
+
+	private var array: [Element] = []
+
+	mutating func removeAll() { array.removeAll() }
+
+	mutating func push(_ element: Element) { array.append(element) }
+
+	@discardableResult mutating func pop() -> Element? { array.popLast() }
+}
+
+extension Stack: CustomStringConvertible
+{
+	var description: String { "\(array)" }
+}
