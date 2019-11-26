c88e3388a3cabfc95975737ce64ef2ce74566a98
Mikhail Medvedev <icodedbear@gmail.com>
2019-11-26 18:23:44 +0300

    Поправил функцию чтобы она была "swiftlint firendly" =)

--- Calculator/Calculator/Models/RPNDecoder.swift
+++ Calculator/Calculator/Models/RPNDecoder.swift
@@ -4,9 +4,7 @@
 //
 //  Created by Mikhail Medvedev on 22.11.2019.
 //  Copyright © 2019 Artem Orlov. All rights reserved.
-// 	swiftlint:disable cyclomatic_complexity
-//  swiftlint:disable force_unwrapping
-//  swiftlint:disable function_body_length
+//
 
 import Foundation
 
@@ -19,40 +17,25 @@ struct RPNDecoder
 
 		for (index, sign) in expressionInArrayFormat.enumerated() {
 			switch sign {
-			case Sign.plus:
-				guard stack.count > 1 else { break }
-				guard let rightOperand = stack.popLast() else { fallthrough }
-				guard let leftOperand = stack.popLast() else { fallthrough }
-				stack.append(String(Double(leftOperand)! + Double(rightOperand)!))
+			case Sign.plus, Sign.minus, Sign.multiply, Sign.divide, Sign.percent:
 
-			case Sign.minus:
 				guard stack.count > 1 else { break }
 				guard let rightOperand = stack.popLast() else { fallthrough }
 				guard let leftOperand = stack.popLast() else { fallthrough }
-				stack.append(String(Double(leftOperand)! - Double(rightOperand)!))
 
-			case Sign.multiply:
-				guard stack.count > 1 else { break }
-			guard let rightOperand = stack.popLast() else { fallthrough }
-				guard let leftOperand = stack.popLast() else { fallthrough }
-				stack.append(String(Double(leftOperand)! * Double(rightOperand)!))
+				guard let leftDouble = Double(leftOperand) else { fallthrough }
+				guard let rightDouble = Double(rightOperand) else { fallthrough }
 
-			case Sign.divide:
-				guard stack.count > 1 else { break }
-				guard let rightOperand = stack.popLast() else { fallthrough }
-				guard let leftOperand = stack.popLast() else { fallthrough }
-				stack.append(String(Double(leftOperand)! / Double(rightOperand)!))
-			case Sign.changeSign:
-				guard stack.count > 1 else { break }
-				guard let rightOperand = stack.popLast() else { fallthrough }
-				stack.removeLast()
-				stack.append(String(Double(rightOperand)!))
-			case Sign.percent:
-				if stack.isEmpty == false {
-				guard let rightOperand = stack.popLast() else { fallthrough }
-				guard let leftOperand = stack.popLast() else { fallthrough }
-				let cachedValue = Double(leftOperand)!
-				let percentFromcachedValue = (Double(leftOperand)! * Double(rightOperand)!) / 100
+				switch sign {
+				case Sign.plus: stack.append(String(leftDouble + rightDouble))
+				case Sign.minus: stack.append(String(leftDouble - rightDouble))
+				case Sign.multiply: stack.append(String(leftDouble * rightDouble))
+				case Sign.divide: stack.append(String(leftDouble / rightDouble))
+
+				case Sign.percent:
+					let cachedValue = leftDouble
+					let percentFromcachedValue = leftDouble * rightDouble / 100
+					// определяем следующий знак в массиве
 					switch expressionInArrayFormat[index + 1]{
 					case Sign.plus:
 						stack.append(String(cachedValue + percentFromcachedValue))
@@ -64,15 +47,24 @@ struct RPNDecoder
 						stack.append(String(cachedValue / percentFromcachedValue))
 					default: break
 					}
+				default: break
 				}
+
+			case Sign.changeSign:
+			//	guard stack.count > 1 else { break }
+				guard let rightOperand = stack.popLast() else { fallthrough }
+				stack.removeLast()
+				if let doubleValue = Double(rightOperand) { stack.append(String(doubleValue)) }
+
 			default:
+				//число, добавляем в стек
 				stack.append(sign)
 			}
 		}
 
 		if stack.isEmpty == false {
-		guard let result = Double(stack.removeLast()) else { return 0 }
-		return result
+			guard let result = Double(stack.removeLast()) else { return 0 }
+			return result
 		}
 		return 0
 	}
