f2934909a5617cc655a90a8ed196804ee7d31966
Arkadiy <dragon_500@mail.ru>
2019-12-01 17:13:33 +0300

    create project

---
+++ MarvelHeroes/MarvelHeroes/MarvelHeroes/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  MarvelHeroes
+//
+//  Created by Arkadiy Grigoryanc on 01.12.2019.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		return true
+	}
+}
+
