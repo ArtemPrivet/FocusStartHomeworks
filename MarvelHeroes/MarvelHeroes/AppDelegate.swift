c625dd75c14bb42641e2288ac2769241d06e975a
belannik <darkmonkz@mail.ru>
2019-12-01 18:31:58 +0300

    Создал пустой проект, добавил свифтлинт и картинки

---
+++ MarvelHeroes/MarvelHeroes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MarvelHeroes
+//
+//  Created by Антон on 01.12.2019.
+//  Copyright © 2019 Anton Belov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate
+{
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication,
+					 didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		window = UIWindow(frame: UIScreen.main.bounds)
+		let mainScreen = MainScreen()
+		window?.rootViewController = mainScreen
+		window?.makeKeyAndVisible()
+		// Override point for customization after application launch.
+		return true
+	}
+}
