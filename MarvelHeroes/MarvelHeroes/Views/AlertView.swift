01e3de333b4c7817762276453a8b61e696c1a0b7
Arkadiy <dragon_500@mail.ru>
2019-12-05 23:57:04 +0300

    add views

---
+++ MarvelHeroes/MarvelHeroes/Views/AlertView.swift
@@ -0,0 +1,68 @@
+//
+//  AlertView.swift
+//  MarvelHeroes
+//
+//  Created by Arkadiy Grigoryanc on 04.12.2019.
+//
+
+import UIKit
+
+final class AlertView: UIView
+{
+	// MARK: Private properties
+	private var titleLabel: UILabel = {
+		let label = UILabel()
+		label.font = UIFont.systemFont(ofSize: 16)
+		label.numberOfLines = 0
+		label.text = "Start typing text"
+		label.textAlignment = .center
+		return label
+	}()
+
+	private let imageView: UIImageView = {
+		let imageView = UIImageView(image: #imageLiteral(resourceName: "search_stub"))
+		imageView.contentMode = .scaleAspectFit
+		return imageView
+	}()
+
+	// MARK: Initialization
+	init() {
+		super.init(frame: .zero)
+		setup()
+	}
+
+	@available(*, unavailable)
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	// MARK: Private methods
+	private func setup() {
+		addSubview(imageView)
+		addSubview(titleLabel)
+
+		setConstraints()
+	}
+
+	private func setConstraints() {
+
+		// Image view constraints
+		imageView.translatesAutoresizingMaskIntoConstraints = false
+		imageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+		imageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+		imageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+		imageView.heightAnchor.constraint(equalTo: widthAnchor).isActive = true
+
+		// Title label constraints
+		titleLabel.translatesAutoresizingMaskIntoConstraints = false
+		titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+		titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
+		titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+		titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+	}
+
+	// MARK: Internal methods
+	func setTitle(_ text: String) {
+		titleLabel.text = text
+	}
+}
---
+++ MarvelHeroes/MarvelHeroes/Views/HeaderView.swift
@@ -0,0 +1,81 @@
+//
+//  HeaderView.swift
+//  MarvelHeroes
+//
+//  Created by Arkadiy Grigoryanc on 05.12.2019.
+//
+
+import UIKit
+
+final class HeaderView: UIView
+{
+	// MARK: Private properties
+	private var titleLabel: UILabel = {
+		let label = UILabel()
+		label.textAlignment = .left
+		label.font = .boldSystemFont(ofSize: 32)
+		label.numberOfLines = 0
+		label.backgroundColor = .clear
+		return label
+	}()
+
+	private lazy var descriptionTextView: UITextView = {
+		let textView = UITextView()
+		textView.textAlignment = .left
+		textView.font = .systemFont(ofSize: 20)
+		textView.isEditable = false
+		textView.isSelectable = false
+		textView.backgroundColor = .clear
+		return textView
+	}()
+
+	private var titleLabelSize: CGSize = .zero
+	private var descriptionTextViewSize: CGSize = .zero
+
+	// MARK: Initializations
+	init(withWidth width: CGFloat, titleText: String?, descriptionText: String?) {
+		super.init(frame: .zero)
+		setup(withWidth: width, title: titleText, description: descriptionText)
+	}
+
+	@available(*, unavailable)
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	// MARK: Private methods
+	private func setup(withWidth width: CGFloat, title: String?, description: String?) {
+		layoutMargins = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+		addSubview(titleLabel)
+		titleLabel.text = title
+		let height = titleLabel
+			.systemLayoutSizeFitting(
+				CGSize(width: width - layoutMargins.left - layoutMargins.right,
+					   height: UIView.layoutFittingCompressedSize.height),
+				withHorizontalFittingPriority: .required,
+				verticalFittingPriority: .fittingSizeLevel)
+			.height
+		titleLabelSize = CGSize(width: width - layoutMargins.left + layoutMargins.right,
+								height: height)
+
+		guard let text = description?.trimmingCharacters(in: .whitespaces), text.isEmpty == false else { return }
+		addSubview(descriptionTextView)
+		descriptionTextView.text = text
+		descriptionTextViewSize = CGSize(width: width - layoutMargins.left - layoutMargins.right,
+										 height: 200)
+	}
+
+	override func layoutSubviews() {
+		titleLabel.frame = CGRect(origin: CGPoint(x: layoutMargins.left,
+												  y: layoutMargins.top),
+								  size: titleLabelSize)
+
+		descriptionTextView.frame = CGRect(origin: CGPoint(x: layoutMargins.left,
+														   y: titleLabel.frame.maxY),
+										   size: descriptionTextViewSize)
+
+		frame = CGRect(origin: .zero,
+					   size: CGSize(width: titleLabelSize.width + layoutMargins.left + layoutMargins.right,
+									height: titleLabelSize.height + descriptionTextViewSize.height + layoutMargins.top + layoutMargins.bottom))
+	}
+}
