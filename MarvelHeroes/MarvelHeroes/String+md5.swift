c705a0ec2e59cf848d9d1d394344622c7f46eade
Arkadiy <dragon_500@mail.ru>
2019-12-06 14:37:13 +0300

    rename folders

--- MarvelHeroes/MarvelHeroes/String+md5.swift
+++ MarvelHeroes/MarvelHeroes/String+md5.swift
@@ -30,7 +30,7 @@ extension String
 		let digestLen = Int(CC_MD5_DIGEST_LENGTH)
 		let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLen)
 
-		CC_MD5(str ?? [], strLen, result)
+		CC_MD5(str, strLen, result)
 
 		let hash = NSMutableString()
 		for index in 0..<digestLen {
