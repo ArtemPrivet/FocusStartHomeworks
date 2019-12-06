43b7675800e79b11a030ff9e809e29b6e131e6fd
Arkadiy <dragon_500@mail.ru>
2019-12-06 13:38:16 +0300

    Switch off CryptoKit

--- MarvelHeroes/MarvelHeroes/MD5.swift
+++ MarvelHeroes/MarvelHeroes/MD5.swift
@@ -7,36 +7,39 @@
 
 import Foundation
 import CommonCrypto
-#if canImport(CryptoKit)
-  import CryptoKit
-#endif
+// Почему то на iOS < 13 приложение крашится при запуске с ошибкой
+// Library not loaded: /System/Library/Frameworks/CryptoKit.framework/CryptoKit
+// и нижеприведенный блок кода не помогает 🤷‍♂️🤷‍♂️🤷‍♂️
+//#if canImport(CryptoKit)
+//  import CryptoKit
+//#endif
 
 extension String
 {
 	var MD5: String {
-		if #available(iOS 13.0, *) {
-			return Insecure
-				.MD5
-				.hash(data: data(using: .utf8) ?? Data())
-				.map { String(format: "%02hhx", $0) }
-				.joined()
+//		if #available(iOS 13.0, *) {
+//			return Insecure
+//				.MD5
+//				.hash(data: data(using: .utf8) ?? Data())
+//				.map { String(format: "%02hhx", $0) }
+//				.joined()
+//		}
+//		else {
+		let str = cString(using: .utf8)
+		let strLen = CC_LONG(lengthOfBytes(using: .utf8))
+		let digestLen = Int(CC_MD5_DIGEST_LENGTH)
+		let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLen)
+
+		CC_MD5(str ?? [], strLen, result)
+
+		let hash = NSMutableString()
+		for index in 0..<digestLen {
+			hash.appendFormat("%02x", result[index])
 		}
-		else {
-			let str = cString(using: .utf8)
-			let strLen = CC_LONG(lengthOfBytes(using: .utf8))
-			let digestLen = Int(CC_MD5_DIGEST_LENGTH)
-			let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLen)
 
-			CC_MD5(str ?? [], strLen, result)
+		result.deinitialize(count: digestLen)
 
-			let hash = NSMutableString()
-			for index in 0..<digestLen {
-				hash.appendFormat("%02x", result[index])
-			}
-
-			result.deinitialize(count: digestLen)
-
-			return String(format: hash as String)
-		}
+		return String(format: hash as String)
+//		}
 	}
 }
