aaf0a0b58d503b8fb7f97662e9312e57628c7d13
Maxim Shalashnikov <schalaschnikov@yandex.ru>
2019-12-06 14:04:36 +0300

    Поправил функцию генерации md5

--- MarvelHeroes/MarvelHeroes/Exprensions.swift
+++ MarvelHeroes/MarvelHeroes/Exprensions.swift
@@ -14,16 +14,12 @@ extension String
 	var md5: String? {
 		let length = Int(CC_MD5_DIGEST_LENGTH)
 		var digest = [UInt8](repeating: 0, count: length)
-
-		if let data = self.data(using: String.Encoding.utf8) {
-			_ = data.withUnsafeBytes{ body in
-				CC_MD5(body, CC_LONG(data.count), &digest)
+		if let data = self.data(using: .utf8) {
+			_ = data.withUnsafeBytes { body -> String in
+				CC_MD5(body.baseAddress, CC_LONG(data.count), &digest)
+				return ""
 			}
-			/*_ = d.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
-				CC_MD5(body, CC_LONG(d.count), &digest)
-			}*/
 		}
-
 		return (0..<length).reduce("") {
 			$0 + String(format: "%02x", digest[$1])
 		}
